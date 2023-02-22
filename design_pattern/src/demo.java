4de917f13f55ae9d095a6af498f5b820b0dc5b47
lindozy <492201845@qq.com>
2023-02-22 21:56:48 +0800

    1.添加哈希算法

---
+++ design_pattern/src/demo.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+/**
+ * @author ：lindo-zy https://github.com/lindo-zy
+ */
+public class demo {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("a", 1);
+        hashMap.put("c", 3);
+        hashMap.put("b", 2);
+        System.out.println(hashMap);
+    }
+}
